ed4c0dfdaf4128bfdcbb69db2533a25941aa4aae
David Arcon. <davidarcon06@gmail.com>
2024-02-02 16:58:00 -0300

    1. Se indenta codigo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-    // Agrega el desplazamiento suave a los enlaces de la barra de navegación.
-$(document).ready(function() {
+// Agrega el desplazamiento suave a los enlaces de la barra de navegación.
+$(document).ready(function () {
     // Selecciono todos los elementos <a> dentro de la clase navbar-nav y agrego controlador para el evento clic.
-    $(".navbar-nav a").on('click', function(event) {
+    $(".navbar-nav a").on('click', function (event) {
         // Valida si el hash del enlace no esta vacio. El hash es la URL que sigue al simbolo #
         if (this.hash !== "") {
             // Se elimina el comportamiento por default del enlace, este seria bajar de forma rapida a la ubicación del Href.
@@ -13,7 +13,7 @@ $(document).ready(function() {
             $('html, body').animate({
                 //Realizo la animación del desplazamiento.
                 scrollTop: $(hash).offset().top
-            }, 800, function(){
+            }, 800, function () {
                 // Con window.locate actualizo la URL en la barra del navegador para reflejar el cambio de posición.
                 window.location.hash = hash;
             });
@@ -21,18 +21,18 @@ $(document).ready(function() {
     });
 });
 // Al presionar el icono o la palabra viajes chile en el nav, la pagina se recarga.
-$(document).ready(function(){
+$(document).ready(function () {
     // Selecciono los elementos #logo y <p> de la clase navbar asigno el evento on, con ello logro que al hacer click sobre alguno de los dos se cumpla la función.
-    $('.navbar #logo, .navbar p').on('click', function(){
+    $('.navbar #logo, .navbar p').on('click', function () {
         // con el metodo reload recargo el elemento location colocar el parametro true por defecto.
         location.reload(true);
-    })   
+    })
 })
 
 // Al presionar el buttom del formulario este debe arrojar mensaje de comprobación de que se envio el formulario y debe reiniciar los input ingresado en cada elemento del formulario.
-$(document).ready(function() {
+$(document).ready(function () {
     // Selecciono el formulario y asigno una funcion al evento de envío del formulario.
-    $("#ConsultaCliente").submit(function(event){
+    $("#ConsultaCliente").submit(function (event) {
         // Evita el comportamiento predeterminado del formulario.
         event.preventDefault();
         // Selecciona el ID del DOM y aplica el efecto fadeIn, el cual muestra el elemento gradualmente.
